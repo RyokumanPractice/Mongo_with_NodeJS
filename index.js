b18cad654cd936d1315836135f0d8a1b58878884
Ryokuman <ryokuman@naver.com>
2022-05-25 18:13:56 +0900

    First Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 8080;
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port, function () {
+    console.log(`listening on ${port}`);
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/src/Pages/Home.html");
+});
+
+app.get("/write", (req, res) => {
+    res.sendFile(__dirname + "/src/Pages/Write.html");
+});
+
+app.post("/add", (req, res) => {
+    res.sendFile(__dirname + "/src/Pages/Add.html");
+    console.log(req.body.title);
+    console.log(req.body.date);
+});
